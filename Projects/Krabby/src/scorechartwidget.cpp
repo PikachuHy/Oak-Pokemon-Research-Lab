6e11bd685f12725841a3f76790067c870b6b2ac3
PikachuHy <pikachuhy@163.com>
2023-05-02 13:07:57 +0800

    [Krabby] add score table

--- Projects/Krabby/src/scorechartwidget.cpp
+++ Projects/Krabby/src/scorechartwidget.cpp
@@ -3,10 +3,24 @@
 #include <QHBoxLayout>
 #include <QWidget>
 #include <QDateTime>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QTabWidget>
+
 ScoreChartWidget::ScoreChartWidget(QWidget* parent): QWidget(parent) {}
 
 void ScoreChartWidget::init(std::vector<Score> scores)
 {
+    auto tabWidget = new QTabWidget();
+    QTableWidget* tableWidget = new QTableWidget();
+    tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    tableWidget->setRowCount(scores.size());
+    tableWidget->setColumnCount(3);
+    QStringList headers;
+    headers << "ID";
+    headers << "SCORE" << "DATE";
+    tableWidget->setHorizontalHeaderLabels(headers);
+
     QSplineSeries *series = new QSplineSeries();
     int max_val = 0;
     for(std::size_t i=0;i<scores.size();i++) {
@@ -14,6 +28,20 @@ void ScoreChartWidget::init(std::vector<Score> scores)
         auto datetime = QDateTime::fromSecsSinceEpoch(scores[i].create_time);
         // series->append(datetime.toMSecsSinceEpoch(), scores[i].score);
         series->append(i, scores[i].score);
+        int col = 0;
+        {
+            auto item = new QTableWidgetItem(QString::number(scores[i].id));
+            tableWidget->setItem(i, col++, item);
+        }
+        {
+            auto item = new QTableWidgetItem(QString::number(scores[i].score));
+            tableWidget->setItem(i, col++, item);
+        }
+        {
+            auto datetime = QDateTime::fromSecsSinceEpoch(scores[i].create_time);
+            auto item = new QTableWidgetItem(datetime.toString("yyyy-MM-dd"));
+            tableWidget->setItem(i, col++, item);
+        }
     }
     max_val = (max_val + 24) / 25 * 25 + 25;
     QChart *chart = new QChart();
@@ -38,7 +66,10 @@ void ScoreChartWidget::init(std::vector<Score> scores)
 
     QChartView *chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
+
+    tabWidget->addTab(chartView, QIcon(":/icon/icons8-line-chart-50.png"), "Chart");
+    tabWidget->addTab(tableWidget, QIcon(":/icon/icons8-spreadsheet-50.png"), "Table");
     auto hbox = new QHBoxLayout();
-    hbox->addWidget(chartView);
+    hbox->addWidget(tabWidget);
     setLayout(hbox);
 }
